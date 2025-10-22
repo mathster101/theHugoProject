9d9ef9fe0486d768e4697163959a91725c964e85
Mathew Panicker <mathewsimonpanicker@gmail.com>
2025-10-22 12:33:16 -0500

    moved all sqlite operations to a class

--- app.py
+++ app.py
@@ -4,43 +4,39 @@ import random
 import sqlite3
 from time import sleep, time 
 import db_operations.database_operations as db
+import db_operations.sqlitedb as sqlitedb
+
 DB_NAME = "mlemdata.db"
 
 def createApp():
     sleep(random.randint(1, 2000) / 999)#just for fun
-    conn = sqlite3.connect(DB_NAME)
-    db.initialize(conn)
-    conn.close()
+    sqliteDB = sqlitedb.SQLiteDB(DB_NAME)
     app = Flask(__name__)
-    return app
+    return app, sqliteDB
 
-app = createApp()
+app, sqliteDB = createApp()
 image_folder = os.path.join(app.root_path, 'static', 'images')
 image_files = os.listdir(image_folder)
 
 @app.route('/', methods = ["GET"])
 def homePage():
-    conn = sqlite3.connect(DB_NAME)
-    db.logVisitTimestamp(conn)
+    sqliteDB.logVisitTimestamp()
     random_string = ''.join(random.choices('abcdefghijklmnopqrstuvwxyz', k=5))
     image_file = random.choice(image_files)
-    visitor_number = db.incrementVisitorCountandReturn(conn)
-    conn.close()
+    visitor_number = sqliteDB.incrementVisitorCountandReturn()
     return render_template('homepage.html', name = random_string, image_file = image_file, visitor_number = visitor_number)
 
 @app.route('/visitorCount', methods = ["GET", "DELETE"])
 def visitorCount():
-    conn = sqlite3.connect(DB_NAME)
     if request.method == "GET":
-        return jsonify({'visitorCount' : db.getVisitorCount(conn)}), 200
+        return jsonify({'visitorCount' : sqliteDB.getVisitorCount()}), 200
     if request.method == "DELETE":
-        db.clearVisitorCount(conn)
+        sqliteDB.clearVisitorCount()
         return '', 204
 
 @app.route('/timestamps', methods = ["GET"])
 def getAllTimestamps():
-    conn = sqlite3.connect(DB_NAME)
-    rows = db.fetchAllTimestamps(conn)
+    rows = sqliteDB.fetchAllTimestamps()
     return rows
 
 @app.route('/timestamps/top/<count>', methods = ["GET"])
---
+++ db_operations/sqlitedb.py
@@ -0,0 +1,95 @@
+import sqlite3
+from time import time
+
+class SQLiteDB:
+    def __init__(self, dbName):
+        self.dbName = dbName
+        self.initializeCounterTable()
+        self.initializeTimestampTable()
+
+    def initializeCounterTable(self):
+        with sqlite3.Connection(self.dbName) as conn:
+            try:
+                cursor = conn.cursor()
+                cursor.execute("""SELECT name FROM sqlite_master
+                            WHERE type='table' AND name='counter'""")
+                if cursor.fetchone() == None:
+                    cursor.execute("""
+                        CREATE TABLE IF NOT EXISTS counter (
+                        id INTEGER PRIMARY KEY,
+                        count INTEGER
+                    )
+                    """)
+                    cursor.execute("INSERT INTO counter VALUES (0,0 )")
+                    conn.commit()
+                    print("table \'counter\' created")
+                else:
+                    print("db already initialized!")
+            except:
+                print("counter db already initialized")
+
+    def initializeTimestampTable(self):
+        with sqlite3.Connection(self.dbName) as conn:
+            try:
+                cursor = conn.cursor()
+                # Create the table if it doesn't already exist
+                cursor.execute('''
+                CREATE TABLE IF NOT EXISTS timestamp (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    unix_time INTEGER UNIQUE,
+                    count INTEGER NOT NULL
+                )
+                ''')
+                conn.commit()
+            except:
+                print("timestamp db already initialized")
+        return
+
+    def getVisitorCount(self):
+        with sqlite3.Connection(self.dbName) as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT count FROM counter where id = 0")
+            current_val = cursor.fetchone()
+            return int(current_val[0])
+        return 
+
+    def clearVisitorCount(self):
+        with sqlite3.Connection(self.dbName) as conn:
+            cursor = conn.cursor()
+            cursor.execute("UPDATE counter SET count = ? WHERE id = 0", (0,))
+            conn.commit()
+        return
+
+    def incrementVisitorCountandReturn(self):
+        with sqlite3.Connection(self.dbName) as conn:
+            cursor = conn.cursor()
+            cursor.execute("""
+                UPDATE counter
+                SET count = count + 1
+                WHERE id = 0
+            """)
+            conn.commit()
+            cursor.execute("SELECT count FROM counter WHERE id = 0")
+            return cursor.fetchone()[0]
+        return
+
+
+    def logVisitTimestamp(self):
+        with sqlite3.Connection(self.dbName) as conn:
+            cursor = conn.cursor()
+            unix_time = int(time())
+            cursor.execute("""
+                INSERT INTO timestamp (unix_time, count)
+                VALUES (?, 1)
+                ON CONFLICT(unix_time) DO UPDATE SET count = count + 1
+            """, (unix_time,))
+            conn.commit()
+        return
+
+    def fetchAllTimestamps(self):
+        with sqlite3.Connection(self.dbName) as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT unix_time, count FROM timestamp")
+            result = cursor.fetchall()
+            return result
+        return
